c21a52918507ce134987e521b985f7ea80e0b49d
Rajasekar <rajasekar241996@gmail.com>
2021-05-22 19:14:20 +0530

    Added header

---
+++ src/common/header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+//Import of stylesheet for header component.
+import './Header.css';
+
+/**
+ * Header Component serves header for multiple pages.
+ */
+
+class Header extends Component {
+
+    render() {
+        return <div className='header-flex-container'>
+            <div>
+                <header className='logo'>Image Viewer</header>
+            </div>
+        </div>
+    }
+}
+
+export default Header;
---
+++ src/screens/login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from "../../common/header/Header";
+
+class Login extends Component {
+    render() {
+        return <div>
+            <div> <Header /> </div>
+            <div> <span>Login Page</span> </div>
+        </div>;
+    }
+}
+
+export default Login;
