76858fe89d8213f6a6a176b2720fd3ea0dc5a766
Rajasekar <rajasekar241996@gmail.com>
2021-05-24 14:48:51 +0530

    few minor fixes

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,12 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 //Import of stylesheet for header component.
 import './Header.css';
 
 //Router import for redirection.
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
-import {Avatar, Divider, IconButton, Input, InputAdornment, Menu, MenuItem, Typography} from "@material-ui/core";
+import { Avatar, Divider, IconButton, Input, InputAdornment, Menu, MenuItem, Typography } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 
@@ -26,7 +26,7 @@ class Header extends Component {
 
     render() {
         if (this.state.loggedOut === true) {
-            return <Redirect to='/'/>
+            return <Redirect to='/' />
         }
         return <div className='header-flex-container'>
             {
@@ -39,36 +39,34 @@ class Header extends Component {
                     :
                     <Fragment>
                         <div onClick={() => this.onLogoClick()}>
-                        <Typography>
                             <header className='logo'>Image Viewer</header>
-                        </Typography>
                         </div>
                         <div className='header-right-flex-container'>
                             {
                                 this.props.showSearchBox ?
                                     <Input className='search-box' type='search' placeholder='Search...' disableUnderline
-                                           startAdornment={
-                                               <InputAdornment position="start"><SearchIcon/></InputAdornment>
-                                           } onChange={this.props.onSearch}/>
+                                        startAdornment={
+                                            <InputAdornment position="start"><SearchIcon /></InputAdornment>
+                                        } onChange={this.props.onSearch} />
                                     :
                                     null
                             }
                             <IconButton id='profile-icon' onClick={this.onProfileIconClick}>
                                 <Avatar variant="circle" alt="profile_picture"
-                                        src={this.props.profilePictureUrl}/>
+                                    src={this.props.profilePictureUrl} />
                             </IconButton>
                             <div>
                                 <Menu open={this.state.menuState} onClose={this.onMenuClose}
-                                      anchorEl={this.state.anchorEl} getContentAnchorEl={null}
-                                      anchorOrigin={{vertical: "bottom", horizontal: "right"}} keepMounted>
-                                  {
+                                    anchorEl={this.state.anchorEl} getContentAnchorEl={null}
+                                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }} keepMounted>
+                                    {
                                         this.props.showMyAccount ?
                                             <MenuItem onClick={this.onMyAccount}><Typography>My
                                                 Account</Typography></MenuItem> : null
                                     }
                                     {
                                         this.props.showMyAccount ?
-                                            <Divider variant="middle"/> : null
+                                            <Divider variant="middle" /> : null
                                     }
                                     <MenuItem onClick={this.onLogout}><Typography>Logout</Typography></MenuItem>
                                 </Menu>
@@ -81,37 +79,37 @@ class Header extends Component {
 
     onLogoClick = () => {
         this.props.history.push({
-                pathname: '/home',
-                state:
-                    {
-                        loginSuccess:true
-                    }
+            pathname: '/home',
+            state:
+            {
+                loginSuccess: true
             }
+        }
         )
-        ;
+            ;
     }
 
     onMyAccount = () => {
         this.props.history.push({
             pathname: '/profile',
             state:
-                {
-                    loginSuccess:true
-                }
+            {
+                loginSuccess: true
+            }
         });
     }
 
     onLogout = () => {
         sessionStorage.removeItem('access-token');
-        this.setState({loggedOut: true})
+        this.setState({ loggedOut: true })
     }
 
     onProfileIconClick = (e) => {
-        this.setState({'menuState': !this.state.menuState, 'anchorEl': e.currentTarget});
+        this.setState({ 'menuState': !this.state.menuState, 'anchorEl': e.currentTarget });
     }
 
     onMenuClose = () => {
-        this.setState({'menuState': !this.state.menuState, 'anchorEl': null});
+        this.setState({ 'menuState': !this.state.menuState, 'anchorEl': null });
     }
 }
 
